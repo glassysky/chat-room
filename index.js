16a1fc683cf5216e4c51d6f59c10242855381259
glassysky <873661157@qq.com>
2016-10-17 21:16:04 +0800

    finished

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+  // console.log(req.ip);
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  // socket.on('disconnect', function(){
+  //   console.log('user disconnected');
+  // });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
